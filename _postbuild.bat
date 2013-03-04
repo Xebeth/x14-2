e38944cb64773981a218f1e14b108b586eef5d23
Xebeth <xebeth@north-edge.com>
2013-03-04 19:51:28 +0000

    commit missing delete/add

--- _postbuild.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-if not exist "J:\Final Fantasy XIV\boot\plugins" mkdir "J:\Final Fantasy XIV\boot\plugins"
-copy "%1plugins\*.dll" "J:\Final Fantasy XIV\boot\plugins"
-if exist "%1WindowerX14.dll" copy "%1WindowerX14.dll" "J:\Final Fantasy XIV\boot\"
