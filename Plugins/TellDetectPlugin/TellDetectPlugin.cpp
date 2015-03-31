4b0a05ac438a789f3004be0acf83f290ea28d14c
Xebeth <xebeth@north-edge.com>
2015-03-31 05:49:32 +0000

    Attempt at avoiding tell sounds when a message is blacklisted

--- Plugins/TellDetectPlugin/TellDetectPlugin.cpp
+++ Plugins/TellDetectPlugin/TellDetectPlugin.cpp
@@ -86,7 +86,7 @@ namespace Windower
 	DWORD TellDetectPlugin::OnChatMessage(USHORT MessageType_in, const char* pSender_in, DWORD MsgSize_in, const char *pOriginalMsg_in,
 										  char **pModifiedMsg_in_out, DWORD ModifiedSize_in, DWORD &MessageFlags_out)
 	{
-		if (MessageType_in == CHAT_MESSAGE_TYPE_INCOMING_TELL_MESSAGE)
+		if (MessageType_in == CHAT_MESSAGE_TYPE_INCOMING_TELL_MESSAGE && MessageFlags_out == MSG_FLAG_NONE)
 			PlaySound(m_SoundFile.c_str(), NULL, SND_FILENAME | SND_ASYNC);
 
 		return MsgSize_in;
